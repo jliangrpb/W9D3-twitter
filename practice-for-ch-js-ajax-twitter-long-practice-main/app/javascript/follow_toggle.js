d397f31047e209ff446274d41531d3d80e19a631
jliangrpb <jeremy.liang612@gmail.com>
2023-05-03 16:30:53 -0400

    Finished the Follow button

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -3,27 +3,44 @@ import { API, broadcast } from "./util";
 export default class FollowToggle {
   constructor(toggleButton) {
     // Your code here
-    this.toggleButton = toggleButton
-
+    this.toggleButton = toggleButton;
+    this.handleClick = this.handleClick.bind(this);
+    this.toggleButton.addEventListener('click', this.handleClick)
     
   }
 
   async handleClick(event) {
     // Your code here
-
+    event.preventDefault();
+    // console.log(this.followState)
+    if (this.followState === "followed") {
+      this.unfollow(); 
+    } else {
+      this.follow();
+    }
   }
 
   async follow() {
     // Your code here
+    console.log('follow is happening')
+    this.followState = "followed"
   }
 
   async unfollow() {
     // Your code here
+    console.log('unfollow is happening')
+    this.followState = "unfollowed"
   }
 
   render() {
     switch (this.followState) {
       // Your code here
+      case "followed":
+        this.toggleButton.innerText = "Unfollow!";
+        break;
+      case "unfollowed":
+        this.toggleButton.innerText = "Follow!";
+        break;
     }
   }
 
